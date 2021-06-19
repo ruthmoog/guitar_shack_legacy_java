05c3f0849817d21853ea5054e04a4f011ba64b7d
Ruth Baker <ruthmoog@gmail.com>
2021-06-19 13:51:03 +0100

    Refactor salesUriBuilder

--- src/main/java/com/guitarshack/SalesUriBuilder.java
+++ src/main/java/com/guitarshack/SalesUriBuilder.java
@@ -1,7 +1,6 @@
 package com.guitarshack;
 
 import java.net.URI;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -10,7 +9,7 @@ import java.util.Map;
 
 public class SalesUriBuilder {
 
-
+    public static final SimpleDateFormat FORMAT = new SimpleDateFormat("M/d/yyyy");
     private final Calendar calendar;
     private final String baseURL;
 
@@ -20,33 +19,32 @@ public class SalesUriBuilder {
     }
 
     public URI getSalesUri(Product product) {
-        Date endDate = getEndDate();
-        Date startDate = getStartDate();
-        DateFormat format = new SimpleDateFormat("M/d/yyyy");
-        Map<String, Object> params1 = new HashMap<>(){{
-            put("productId", product.getId());
-            put("startDate", format.format(startDate));
-            put("endDate", format.format(endDate));
-            put("action", "total");
-        }};
-        String paramString1 = "?";
+        Map<String, Object> queryParameters = buildQueryParameters(product);
+        String queryString = buildQueryString(queryParameters);
+        return URI.create(baseURL + queryString);
+    }
 
-        for (String key : params1.keySet()) {
-            paramString1 += key + "=" + params1.get(key).toString() + "&";
+    private String buildQueryString(Map<String, Object> queryParameters) {
+        String queryString = "?";
+        for (String key : queryParameters.keySet()) {
+            queryString += key + "=" + queryParameters.get(key).toString() + "&";
         }
-        String url = baseURL + paramString1;
-        System.out.println("url: " + url);
-        return URI.create(url);
+        return queryString;
+    }
+
+    private Map<String, Object> buildQueryParameters(Product product) {
+        return new HashMap<>(){{
+                put("productId", product.getId());
+                put("startDate", FORMAT.format(getStartDate()));
+                put("endDate", FORMAT.format(calendar.getTime()));
+                put("action", "total");
+            }};
     }
 
     private Date getStartDate() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(this.calendar.getTime());
         calendar.add(Calendar.DATE, -30);
-        return calendar.getTime();
-    }
-
-    private Date getEndDate() {
         return calendar.getTime();
     }
 }
